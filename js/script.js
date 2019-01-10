6458c08ffd2362690e9b42ffd52bbe0f463af5ba
Rafal Pijet <stronglopez@wp.pl>
2019-01-10 11:33:12 +0100

    Adding recommendation from task 14.2

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
 "use strict";
 (function() {
     var elem = document.querySelector(".main-carousel");
+    var result = document.getElementById("carousel");
     var restartButton = document.querySelector(".restart");
     var progressBar = document.querySelector(".progress-bar");
+    var template = document.getElementById("template-slide-item").innerHTML;
+    Mustache.parse(template);
+
+    for (var i = 0; i < slideData.length; i++) {
+        var generatedSlide = Mustache.render(template, slideData[i]);
+        result.insertAdjacentHTML("beforeEnd", generatedSlide);
+    }
+    
     var flkty = new Flickity(elem, {
         cellAlign: "left",
         contain: true,
