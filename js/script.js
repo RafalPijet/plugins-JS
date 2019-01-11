fbba27ce7d1149efbb5a0671f949242afc25414e
Rafal Pijet <stronglopez@wp.pl>
2019-01-11 14:44:09 +0100

    Adding recommendation from task 14.4

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@
     var restartButton = document.querySelector(".restart");
     var progressBar = document.querySelector(".progress-bar");
     var template = document.getElementById("template-slide-item").innerHTML;
+    var map = null;
+    var check = true;
     Mustache.parse(template);
 
     for (var i = 0; i < slideData.length; i++) {
@@ -24,18 +26,42 @@
         flkty.selectCell(0);
     })
 
+    result.addEventListener("mousemove", function() {
+        check = true;
+    })
+
     flkty.on("scroll", function(progress) {
         progress = Math.max(0, Math.min(1, progress));
         progressBar.style.width = progress * 100 + "%";
     })
 
+    flkty.on("change", function (index) {
+        if (check) {
+            map.panTo(slideData[index].coords);
+            map.setZoom(16);
+        } else {
+            map.setZoom(12);
+        }
+    })
+
+    function createMarker(pos, i) {
+        var marker = new google.maps.Marker({
+            position: pos,
+            map: map
+        });
+        google.maps.event.addListener(marker, "click", function () {
+            check = false;
+            map.setZoom(12);
+            flkty.selectCell(i);
+        });
+    }
+
     window.initMap = function() {
-        var map = new google.maps.Map(
+        map = new google.maps.Map(
             document.getElementById('map'), {zoom: 12, center: slideData[0].coords});
 
         for (var i = 0; i < slideData.length; i++) {
-            new google.maps.Marker({position: slideData[i].coords, map: map});
+            createMarker(slideData[i].coords, i);
         }
     }
-
 })();
